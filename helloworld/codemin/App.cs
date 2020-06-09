e90123c2ee7cdaf303212fffe9afb964cf34337d
huobur <huobur@outlook.com>
2020-06-09 13:18:56 -0700

    Initial commit

---
+++ helloworld/codemin/App.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace codemin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main() {
+            var win = new Window();
+            var app = new App();
+            var tb = new TextBlock();
+			
+            tb.Text="Hello, World!";
+            win.Content = tb;
+			
+            win.Show();
+            app.MainWindow = win;
+            app.Run();
+
+        }
+    }
+}
